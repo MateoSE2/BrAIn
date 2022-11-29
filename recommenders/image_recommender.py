0af793669f8a62fbbdd525389287523f8ce28954
MateoSesmaUrbano <mateo.sesma.u@gmail.com>
2022-11-29 16:29:41 +0100

    image_recommender

---
+++ recommenders/image_recommender.py
@@ -0,0 +1,166 @@
+import os
+from PIL import Image
+from torchvision import transforms
+import torch
+from tqdm import tqdm
+from torchvision import models
+from torchvision.models import resnet18, ResNet18_Weights
+import pandas as pd
+import numpy as np
+from numpy.testing import assert_almost_equal
+import pickle
+
+# Create the input images for the CNN
+inputDim = (224,224)
+root = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
+inputDir = os.path.join(root, 'images/')
+inputDirCNN = "inputCNN"
+
+# if the directory does not exist, create it
+if not os.path.exists(inputDirCNN):
+    os.makedirs(inputDirCNN)
+
+transformationForCNNInput = transforms.Compose([
+                                                transforms.Resize(inputDim),
+                                            ])
+
+# load the images, resize them and save them in the inputDirCNN 
+images_list = os.listdir(inputDir)
+for imageName in os.listdir(inputDir):
+    I = Image.open(os.path.join(inputDir, imageName))
+    newI = transformationForCNNInput(I)
+
+    # copy the rotation information metadata from original image and save, else your transformed images may be rotated
+    newI.save(os.path.join(inputDirCNN, imageName))
+    
+    newI.close()
+    I.close()
+
+# for this prototype we use no gpu, cuda= False and as model resnet18 to obtain feature vectors
+
+class Img2VecResnet18():
+    def __init__(self):
+        
+        self.device = torch.device("cpu")
+        self.numberFeatures = 512
+        self.modelName = "resnet-18"
+        self.model, self.featureLayer = self.getFeatureLayer()
+        self.model = self.model.to(self.device)
+        self.model.eval()
+        self.toTensor = transforms.ToTensor()
+        
+        # normalize the resized images as expected by resnet18
+        # [0.485, 0.456, 0.406] --> normalized mean value of ImageNet, [0.229, 0.224, 0.225] std of ImageNet
+        self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        
+    def getVec(self, img):
+        image = self.normalize(self.toTensor(img)).unsqueeze(0).to(self.device)
+        embedding = torch.zeros(1, self.numberFeatures, 1, 1)
+
+        def copyData(m, i, o): embedding.copy_(o.data)
+
+        h = self.featureLayer.register_forward_hook(copyData)
+        self.model(image)
+        h.remove()
+
+        return embedding.numpy()[0, :, 0, 0]
+
+    def getFeatureLayer(self):
+        
+        cnnModel = models.resnet18(weights= ResNet18_Weights.DEFAULT)
+        layer = cnnModel._modules.get('avgpool')
+        self.layer_output_size = 512
+        
+        return cnnModel, layer
+
+# create the image vectors for the images in the inputDirCNN
+img2vec = Img2VecResnet18() 
+
+allVectors = {}
+print("Converting images to feature vectors:")
+for image in tqdm(os.listdir(inputDirCNN)):
+    I = Image.open(os.path.join(inputDirCNN, image))
+    vec = img2vec.getVec(I)
+    allVectors[image] = vec
+    I.close() 
+
+# now let us define a function that calculates the cosine similarity entries in the similarity matrix
+
+def getSimilarityMatrix(vectors):
+    v = np.array(list(vectors.values())).T
+    sim = np.inner(v.T, v.T) / ((np.linalg.norm(v, axis=0).reshape(-1,1)) * ((np.linalg.norm(v, axis=0).reshape(-1,1)).T))
+    keys = list(vectors.keys())
+    matrix = pd.DataFrame(sim, columns = keys, index = keys)
+    
+    return matrix
+        
+similarityMatrix = getSimilarityMatrix(allVectors)
+
+k = int(input("Enter the number of images to retrieve: "))
+
+similarNames = pd.DataFrame(index = similarityMatrix.index, columns = range(k))
+similarValues = pd.DataFrame(index = similarityMatrix.index, columns = range(k))
+
+for j in tqdm(range(similarityMatrix.shape[0])):
+    kSimilar = similarityMatrix.iloc[j, :].sort_values(ascending = False).head(k)
+    similarNames.iloc[j, :] = list(kSimilar.index)
+    similarValues.iloc[j, :] = kSimilar.values
+    
+similarNames.to_pickle("similarNames.pkl")
+similarValues.to_pickle("similarValues.pkl")
+
+import matplotlib.pyplot as plt
+    
+numCol = k
+numRow = 1
+
+def setAxes(ax, image, query = False, **kwargs):
+    value = kwargs.get("value", None)
+    if query:
+        ax.set_xlabel("Query Image\n{0}".format(image), fontsize = 12)
+    else:
+        ax.set_xlabel("Similarity value {1:1.3f}\n{0}".format( image,  value), fontsize = 12)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    
+def getSimilarImages(image, simNames, simVals):
+    if image in set(simNames.index):
+        imgs = list(simNames.loc[image, :])
+        vals = list(simVals.loc[image, :])
+        if image in imgs:
+            assert_almost_equal(max(vals), 1, decimal = 5)
+            imgs.remove(image)
+            vals.remove(max(vals))
+        return imgs, vals
+    else:
+        print("'{}' Unknown image".format(image))
+        
+def plotSimilarImages(image, simiarNames, similarValues):
+    simImages, simValues = getSimilarImages(image, similarNames, similarValues)
+    fig = plt.figure(figsize=(10, 20))
+    
+    # now plot the  most simliar images
+    for j in range(0, numCol*numRow):
+        ax = []
+        if j == 0:
+            img = Image.open(os.path.join(inputDir, image))
+            ax = fig.add_subplot(numRow, numCol, 1)
+            setAxes(ax, image, query = True)
+        else:
+            img = Image.open(os.path.join(inputDir, simImages[j-1]))
+            ax.append(fig.add_subplot(numRow, numCol, j+1))
+            setAxes(ax[-1], simImages[j-1], value = simValues[j-1])
+        img = img.convert('RGB')
+        plt.imshow(img)
+        img.close()
+    
+    plt.show()
+
+# take three examples from the provided image set and plot
+inputImages = input("Enter the names of the images to retrieve similar images: ").split()
+
+for image in (inputImages):
+    try:
+        plotSimilarImages(image, similarNames, similarValues)
+    except:
+        pass
